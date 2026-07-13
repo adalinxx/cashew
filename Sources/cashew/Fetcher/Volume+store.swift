6528a859ef1627ecb237a21b8ea92f0c7e3a7e04
adalinxx <ada_lin_lattice@proton.me>
2026-07-13 13:20:20 -0500

    preserve encrypted volume root bytes

--- Sources/cashew/Fetcher/Volume+store.swift
+++ Sources/cashew/Fetcher/Volume+store.swift
@@ -1,3 +1,6 @@
+import Crypto
+import Foundation
+
 public extension Volume {
     func storeRecursively(storer: Storer) throws {
         // Calling storage on a Volume root is a request to publish that complete
@@ -5,11 +8,24 @@ public extension Volume {
         // nested Volumes are skipped by Node.storeRecursively because each nested
         // boundary is an independent availability unit.
         guard let node = node else { throw DataErrors.nodeNotAvailable }
-        guard let nodeData = node.toData() else { throw DataErrors.serializationFailed }
+
+        let dataToStore: Data
+        if let info = encryptionInfo {
+            guard let keyProvider = storer as? KeyProvider else { throw DataErrors.keyNotFound }
+            guard let key = keyProvider.key(for: info.keyHash) else { throw DataErrors.keyNotFound }
+            guard let ivData = info.ivData else { throw DataErrors.invalidIV }
+            let nonce = try AES.GCM.Nonce(data: ivData)
+            let plaintext = try Self.serializeNode(node, codec: Self.defaultCodec)
+            dataToStore = try EncryptionHelper.encrypt(data: plaintext, key: key, nonce: nonce)
+        } else {
+            guard let nodeData = node.toData() else { throw DataErrors.serializationFailed }
+            dataToStore = nodeData
+        }
+
         if let volumeAware = storer as? VolumeAwareStorer {
             try volumeAware.enterVolume(rootCID: rawCID)
             do {
-                try volumeAware.store(rawCid: rawCID, data: nodeData)
+                try volumeAware.store(rawCid: rawCID, data: dataToStore)
                 try node.storeRecursively(storer: volumeAware)
                 try volumeAware.exitVolume(rootCID: rawCID)
             } catch {
@@ -19,7 +35,7 @@ public extension Volume {
                 throw error
             }
         } else {
-            try storer.store(rawCid: rawCID, data: nodeData)
+            try storer.store(rawCid: rawCID, data: dataToStore)
             try node.storeRecursively(storer: storer)
         }
     }
